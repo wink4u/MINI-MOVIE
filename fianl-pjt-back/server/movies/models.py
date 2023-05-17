b3475850af861425dbe379d95c5c8e5386c4ea7c
= <=>
2023-05-17 17:43:40 +0900

    hi

--- fianl-pjt-back/server/movies/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.conf import settings
-# Create your models here.
-
-class Movie(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    content = models.TextField(max_length=100)
-    
