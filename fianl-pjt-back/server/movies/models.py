179a5ae256f4fa05abb08e17a696b68dfa9464d5
= <=>
2023-05-17 15:09:23 +0900

    hihi

--- fianl-pjt-back/server/movies/models.py
+++ fianl-pjt-back/server/movies/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max-_length=100)
+    content = models.TextField()
--- fianl-pjt-back/server/movies/views.py
+++ fianl-pjt-back/server/movies/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# # from .serializers import 
+# from rest_framework.decorators import permission_classes
+# from rest_framework.permissions import IsAuthenticated 
+# # Create your views here.
+
+# @api_view
