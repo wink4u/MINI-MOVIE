3ab9a3d5d119b65e77f90900f23ac8b44d2478b3
이정찬 <wjdcks1203@naver.com>
2023-05-17 11:56:58 +0900

    1

--- final-pjt-front/client/src/App.vue
+++ final-pjt-front/client/src/App.vue
@@ -1,8 +1,29 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">로그인</a>
+            </li>
+            <li class="nav-item">
+              <button class="nav-link" href="#">회원가입</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/normal">일반</router-link> |
+      <router-link to="/concept">특성</router-link>
     </nav>
     <router-view/>
   </div>
