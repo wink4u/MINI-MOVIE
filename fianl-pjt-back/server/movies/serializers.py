c3026a65fa0c6c397dcc4e914507f5afdf761781
= <=>
2023-05-18 15:40:46 +0900

    hi

--- fianl-pjt-back/server/movies/serializers.py
+++ fianl-pjt-back/server/movies/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie, Genre
 
 class MovieListSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = '(title, overview, poster_path)'
+        
+
+# movie Genre_id에 따라서 Genre를 불러오기위한 Serializer
+class GenreSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Genre
+        fields = ('name')
         
 
 class MovieDetailSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
     
     class Meta:
         model = Movie
